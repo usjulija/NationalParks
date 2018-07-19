98d05e501368a7fb5198e3536052b7489a44c918
Julia <us.julija@gmail.com>
2018-07-19 19:10:06 +0300

    places added

--- README.md
+++ README.md
@@ -1,3 +1,8 @@
+Using Google Map in React Component
+https://stackoverflow.com/questions/48493960/using-google-map-in-react-component
 
 Creating a Sliding Menu in React by kirupa
 https://www.kirupa.com/react/smooth_sliding_menu_react_motion.htm
+
+20 of the Best National Parks in Europe
+http://www.worldofwanderlust.com/20-of-the-best-national-parks-in-europe/
--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+  /* global google */
 import React, { Component } from 'react';
 import { Route } from 'react-router-dom';
 import './App.css';
@@ -6,8 +7,14 @@ import Footer from './components/Footer';
 import MenuContainer from './components/MenuContainer';
 
 class App extends Component {
-  state = {
-    visible: false
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      visible: false,
+      mylocations: require("./data/places.json"), // Get the places from the JSON file in data folder
+    };
+    this.toggleNavMenu = this.toggleNavMenu.bind(this);
   }
 
   toggleNavMenu() {
@@ -15,6 +22,68 @@ class App extends Component {
         visible: !this.state.visible
     });
   }
+
+//getGoogleMaps() was provided by tremby at: https://stackoverflow.com/a/48494032
+  getGoogleMaps() {
+    // Defines the promise
+    if (!this.googleMapsPromise) {
+      this.googleMapsPromise = new Promise((resolve) => {
+        // Adds a global handler for when the API finishes loading
+        window.resolveGoogleMapsPromise = () => {
+          // Resolves the promise
+          resolve(google);
+
+          // Tidy up
+          delete window.resolveGoogleMapsPromise;
+        };
+
+        // Loads the Google Maps API
+        const script = document.createElement("script");
+        const API = 'AIzaSyAseJ92xDY6RipMXcHJ4jdP72SZGyM-drE';
+        script.src = `https://maps.googleapis.com/maps/api/js?key=${API}&callback=resolveGoogleMapsPromise`;
+        script.async = true;
+        document.body.appendChild(script);
+      });
+    }
+
+    // Returns a promise for the Google Maps API
+    return this.googleMapsPromise;
+  }
+
+  componentWillMount() {
+    // Start Google Maps API loading since we know we'll soon need it
+    this.getGoogleMaps();
+  }
+
+  componentDidMount() {
+    // Once the Google Maps API has finished loading, initializes the map
+    this.getGoogleMaps().then((google) => {
+      const uluru = {lat: 54.5260, lng: 15.2551};
+      const map = new google.maps.Map(document.getElementById('map'), {
+        zoom: 4,
+        center: uluru
+      });
+
+      let markers = [];
+      this.state.mylocations.forEach((item) => {
+        let position = item.latlng;
+        let title = item.name;
+        let markerID = item.id;
+
+        let marker = new google.maps.Marker({
+          map: map,
+          position: position,
+          title: title,
+          animation: google.maps.Animation.DROP,
+          id: markerID
+        });
+
+        // Push the marker to the array of markers.
+        markers.push(marker);
+      })
+    });
+  }
+
 
   render() {
     let sliding = this.state.visible === true ? "slide" : "unslide";
@@ -26,7 +95,7 @@ class App extends Component {
         <Route exact path="/" render={() => (
           <div>
             <MenuContainer
-              onMouseDown={this.toggleNavMenu.bind(this)}
+              onMouseDown={this.toggleNavMenu}
               visible={this.state.visible}
             />
             <div id="main-view" className={sliding}>
@@ -35,7 +104,7 @@ class App extends Component {
                 <svg
                   className={hiding}
                   tabIndex={tabindex}
-                  onClick={this.toggleNavMenu.bind(this)}
+                  onClick={this.toggleNavMenu}
                   role="button"
                   aria-label="open sidebar"
                   alt="menu icon"
@@ -48,6 +117,7 @@ class App extends Component {
                   </g>
                 </svg>
               </nav>
+              <div id="map"></div>
               <Footer />
             </div>
           </div>
